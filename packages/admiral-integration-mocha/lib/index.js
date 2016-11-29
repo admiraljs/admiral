b084e0cd2e8cb969569f685829ab851700714a36
Jarid Margolin <jaridmargolin@gmail.com>
2016-11-29 00:41:08 -0500

    Init commit.

---
+++ packages/admiral-integration-mocha/lib/index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+/* -----------------------------------------------------------------------------
+ * dependencies
+ * ---------------------------------------------------------------------------*/
+
+// core
+const path = require('path');
+
+// 3rd party
+const _ = require('lodash');
+const Promise = require('bluebird');
+const Mocha = require('mocha');
+
+
+/* -----------------------------------------------------------------------------
+ * IntegrationMochaRunner
+ * ---------------------------------------------------------------------------*/
+
+const Runner = module.exports = function (options) {
+  this.options = options;
+};
+
+Runner.options = {
+  'integrationMocha.target': {
+    type: 'string',
+    description: 'Key representing the target the tests will execute in. By default `targetLocal` is the only existing target. Additional targets may be loaded using plugins.',
+    default: 'targetLocal.chrome'
+  }
+};
+
+Runner.prototype.run = function (test, browsers) {
+  return Promise.mapSeries(browsers, (browser) => this.runTest(test, browser));
+};
+
+Runner.prototype.runTest = function (test, browser) {
+  const attachDriver = function () {
+    // ensure we have root suite
+    let suite = this.suite;
+    while(suite.parent) { suite = suite.parent; }
+
+    Object.defineProperty(suite.ctx, "driver", {
+      get: function() { return browser.driver; }
+    });
+  };
+
+  return new Promise(function (resolve, reject) {
+    const mocha = new Mocha().delay();
+
+    _.each(test.files, (file) => {
+      delete require.cache[path.resolve(file)];
+      mocha.addFile(file);
+    });
+
+    const runner = mocha.run((failures) => {
+      return browser.quit().then(resolve(failures));
+    });
+
+    runner.on('start', attachDriver);
+    runner.suite.run();
+  });
+};
---
+++ packages/admiral-integration-mocha/test/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/* -----------------------------------------------------------------------------
+ * dependencies
+ * ---------------------------------------------------------------------------*/
+
+// 3rd party
+const assert = require('chai').assert;
+const Promise = require('bluebird');
+
+// lib
+const Runner = require('../lib/index');
+
+
+/* -----------------------------------------------------------------------------
+ * IntegrationMochaRunner
+ * ---------------------------------------------------------------------------*/
+
+const Browser = function () { this.driver = 'test'; };
+Browser.prototype.quit = function () { return Promise.resolve(); };
+
+const chrome = new Browser();
+const firefox = new Browser();
+const runner = new Runner();
+
+runner.run({
+  files: ['./test/test.js', './test/multiple.js']
+}, [chrome, firefox]).then((result) => {
+  assert.equal(result.length, 2);
+});
