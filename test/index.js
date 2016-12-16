a463d453ee1ff00f9721ae73156af977fc4e3d57
Jarid Margolin <jaridmargolin@gmail.com>
2016-12-16 11:09:41 -0500

    Use standard coding style.

--- test/index.js
+++ test/index.js
@@ -1,184 +1,180 @@
-'use strict';
+/* eslint-env mocha */
+'use strict'
 
 /* -----------------------------------------------------------------------------
  * dependencies
- * ---------------------------------------------------------------------------*/
+ * -------------------------------------------------------------------------- */
 
 // core
-const spawn = require('child_process').spawn;
-const path = require('path');
-const fs = require('fs-extra');
+const path = require('path')
+const fs = require('fs-extra')
 
 // 3rd party
-const assert = require('chai').assert;
-const sinon = require('sinon');
+const assert = require('chai').assert
+const sinon = require('sinon')
 
 // lib
-const admiral = require('../lib/index');
-const unitPlugin = require('./fixtures/example/node_modules/admiral-unit-plugin');
-const integrationPlugin = require('./fixtures/example/node_modules/admiral-integration-plugin');
-
+const admiral = require('../lib/index')
+const unitPlugin = require('./fixtures/example/node_modules/admiral-unit-plugin')
+const integrationPlugin = require('./fixtures/example/node_modules/admiral-integration-plugin')
 
 /* -----------------------------------------------------------------------------
  * reusable
- * ---------------------------------------------------------------------------*/
+ * -------------------------------------------------------------------------- */
 
-const exampleRoot = path.join(__dirname, 'fixtures', 'example');
-
+const exampleRoot = path.join(__dirname, 'fixtures', 'example')
 
 /* -----------------------------------------------------------------------------
  * test
- * ---------------------------------------------------------------------------*/
+ * -------------------------------------------------------------------------- */
 
 describe('admiral', function () {
-
   before(function () {
-    this.ogCwd = process.cwd();
-    process.chdir(exampleRoot);
-  });
+    this.ogCwd = process.cwd()
+    process.chdir(exampleRoot)
+  })
 
   after(function () {
-    process.chdir(this.ogCwd);
-  });
+    process.chdir(this.ogCwd)
+  })
 
   // Our tests depend on checking for the existence of admiralfile. This
   // ensure state is not persisted.
   beforeEach(function () {
-    delete require.cache[path.join(exampleRoot, 'admiralfile.js')];
-  });
+    delete require.cache[path.join(exampleRoot, 'admiralfile.js')]
+  })
 
   it('Should run both unit and integration tests.', function () {
     return admiral.execute().then((result) => {
-      assert.deepEqual(result, ['unit', 'integration']);
-    });
-  });
+      assert.deepEqual(result, ['unit', 'integration'])
+    })
+  })
 
   it('Should only run if tests are defined.', function () {
-    fs.copySync('test/unit', 'test/_unit');
-    fs.removeSync('test/unit');
+    fs.copySync('test/unit', 'test/_unit')
+    fs.removeSync('test/unit')
 
     return admiral.execute().then((result) => {
-      assert.deepEqual(result, ['integration']);
+      assert.deepEqual(result, ['integration'])
 
-      fs.copySync('test/_unit', 'test/unit');
-      fs.removeSync('test/_unit');
-    });
-  });
+      fs.copySync('test/_unit', 'test/unit')
+      fs.removeSync('test/_unit')
+    })
+  })
 
   it('Should instantiate target and runner with options.', function () {
-    const unitSpy = sinon.spy(unitPlugin.prototype, 'run');
-    const integrationSpy = sinon.spy(integrationPlugin.prototype, 'run');
+    const unitSpy = sinon.spy(unitPlugin.prototype, 'run')
+    const integrationSpy = sinon.spy(integrationPlugin.prototype, 'run')
 
     return admiral.execute().then(() => {
-      const unitTest = unitSpy.args[0][0];
-      assert.deepEqual(unitTest.runner.options, { 'target': 'target' });
-      assert.deepEqual(unitTest.target.options, { 'defaultBrowser': 'chrome' });
+      const unitTest = unitSpy.args[0][0]
+      assert.deepEqual(unitTest.runner.options, { 'target': 'target' })
+      assert.deepEqual(unitTest.target.options, { 'defaultBrowser': 'chrome' })
 
-      const integrationTest = integrationSpy.args[0][0];
-      assert.deepEqual(integrationTest.runner.options, { 'target': 'target' });
-      assert.deepEqual(integrationTest.target.options, { 'defaultBrowser': 'chrome' });
+      const integrationTest = integrationSpy.args[0][0]
+      assert.deepEqual(integrationTest.runner.options, { 'target': 'target' })
+      assert.deepEqual(integrationTest.target.options, { 'defaultBrowser': 'chrome' })
 
-      unitSpy.restore();
-      integrationSpy.restore();
-    });
-  });
+      unitSpy.restore()
+      integrationSpy.restore()
+    })
+  })
 
   it('Should pass test object and devices to `run`.', function () {
-    const unitSpy = sinon.spy(unitPlugin.prototype, 'run');
+    const unitSpy = sinon.spy(unitPlugin.prototype, 'run')
 
     return admiral.execute().then(() => {
-      const test = unitSpy.args[0][0];
-      const devices = unitSpy.args[0][1];
-      assert.deepEqual(test.files, ['test/unit/index.js', 'test/unit/index.spec.js']);
-      assert.equal(test.target.name, 'target');
-      assert.deepEqual(devices, ['device']);
+      const test = unitSpy.args[0][0]
+      const devices = unitSpy.args[0][1]
+      assert.deepEqual(test.files, ['test/unit/index.js', 'test/unit/index.spec.js'])
+      assert.equal(test.target.name, 'target')
+      assert.deepEqual(devices, ['device'])
 
-      unitSpy.restore();
-    });
-  });
+      unitSpy.restore()
+    })
+  })
 
   it('Should filter files based on `tests` glob.', function () {
-    const unitSpy = sinon.spy(unitPlugin.prototype, 'run');
+    const unitSpy = sinon.spy(unitPlugin.prototype, 'run')
 
     return admiral.execute({
       'unit.glob': '**/*.spec.js'
     }).then(() => {
-      assert.deepEqual(unitSpy.args[0][0].files, ['test/unit/index.spec.js']);
+      assert.deepEqual(unitSpy.args[0][0].files, ['test/unit/index.spec.js'])
 
-      unitSpy.restore();
-    });
-  });
+      unitSpy.restore()
+    })
+  })
 
   it('Should throw error if test runner specified but not installed.', function () {
-    const ogPkg = fs.readFileSync('package.json', { encoding: 'utf-8' });
-    const newPkg = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf-8' }));
+    const ogPkg = fs.readFileSync('package.json', { encoding: 'utf-8' })
+    const newPkg = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf-8' }))
 
-    delete newPkg.dependencies['admiral-unit-plugin'];
-    fs.writeFileSync('package.json', JSON.stringify(newPkg));
-    fs.copySync('node_modules/admiral-unit-plugin', 'node_modules/_admiral-unit-plugin');
-    fs.removeSync('node_modules/admiral-unit-plugin');
+    delete newPkg.dependencies['admiral-unit-plugin']
+    fs.writeFileSync('package.json', JSON.stringify(newPkg))
+    fs.copySync('node_modules/admiral-unit-plugin', 'node_modules/_admiral-unit-plugin')
+    fs.removeSync('node_modules/admiral-unit-plugin')
 
     return admiral.execute().catch((e) => {
-      assert.equal(e.message, 'Specified unit runner not installed.');
+      assert.equal(e.message, 'Specified unit runner not installed.')
 
-      fs.copySync('node_modules/_admiral-unit-plugin', 'node_modules/admiral-unit-plugin');
-      fs.removeSync('node_modules/_admiral-unit-plugin');
-      fs.writeFileSync('package.json', ogPkg);
-    });
-  });
+      fs.copySync('node_modules/_admiral-unit-plugin', 'node_modules/admiral-unit-plugin')
+      fs.removeSync('node_modules/_admiral-unit-plugin')
+      fs.writeFileSync('package.json', ogPkg)
+    })
+  })
 
   it('Should infer test runners if not explicitly specified.', function () {
-    fs.copySync('admiralfile.js', '_admiralfile.js');
-    fs.removeSync('admiralfile.js');
+    fs.copySync('admiralfile.js', '_admiralfile.js')
+    fs.removeSync('admiralfile.js')
 
     return admiral.execute().then((result) => {
-      assert.deepEqual(result, ['unit', 'integration']);
+      assert.deepEqual(result, ['unit', 'integration'])
 
-      fs.copySync('_admiralfile.js', 'admiralfile.js');
-      fs.removeSync('_admiralfile.js');
-    });
-  });
+      fs.copySync('_admiralfile.js', 'admiralfile.js')
+      fs.removeSync('_admiralfile.js')
+    })
+  })
 
   it('Should throw error if test runner not specified or loaded.', function () {
-    const ogPkg = fs.readFileSync('package.json', { encoding: 'utf-8' });
-    const newPkg = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf-8' }));
+    const ogPkg = fs.readFileSync('package.json', { encoding: 'utf-8' })
+    const newPkg = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf-8' }))
 
-    delete newPkg.dependencies['admiral-unit-plugin'];
-    fs.writeFileSync('package.json', JSON.stringify(newPkg));
-    fs.copySync('node_modules/admiral-unit-plugin', 'node_modules/_admiral-unit-plugin');
-    fs.removeSync('node_modules/admiral-unit-plugin');
-    fs.copySync('admiralfile.js', '_admiralfile.js');
-    fs.removeSync('admiralfile.js');
+    delete newPkg.dependencies['admiral-unit-plugin']
+    fs.writeFileSync('package.json', JSON.stringify(newPkg))
+    fs.copySync('node_modules/admiral-unit-plugin', 'node_modules/_admiral-unit-plugin')
+    fs.removeSync('node_modules/admiral-unit-plugin')
+    fs.copySync('admiralfile.js', '_admiralfile.js')
+    fs.removeSync('admiralfile.js')
 
     return admiral.execute().catch((e) => {
-      assert.equal(e.message, 'Must install a unit runner.');
+      assert.equal(e.message, 'Must install a unit runner.')
 
-      fs.copySync('node_modules/_admiral-unit-plugin', 'node_modules/admiral-unit-plugin');
-      fs.removeSync('node_modules/_admiral-unit-plugin');
-      fs.writeFileSync('package.json', ogPkg);
-      fs.copySync('_admiralfile.js', 'admiralfile.js');
-      fs.removeSync('_admiralfile.js');
-    });
-  });
+      fs.copySync('node_modules/_admiral-unit-plugin', 'node_modules/admiral-unit-plugin')
+      fs.removeSync('node_modules/_admiral-unit-plugin')
+      fs.writeFileSync('package.json', ogPkg)
+      fs.copySync('_admiralfile.js', 'admiralfile.js')
+      fs.removeSync('_admiralfile.js')
+    })
+  })
 
   it('Should throw error if multiple runners installed but neither specified.', function () {
-    const ogPkg = fs.readFileSync('package.json', { encoding: 'utf-8' });
-    const newPkg = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf-8' }));
+    const ogPkg = fs.readFileSync('package.json', { encoding: 'utf-8' })
+    const newPkg = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf-8' }))
 
-    newPkg.dependencies['admiral-unit-plugin2'] = "^0.0.1";
-    fs.writeFileSync('package.json', JSON.stringify(newPkg));
-    fs.copySync('node_modules/admiral-unit-plugin', 'node_modules/admiral-unit-plugin2');
-    fs.copySync('admiralfile.js', '_admiralfile.js');
-    fs.removeSync('admiralfile.js');
+    newPkg.dependencies['admiral-unit-plugin2'] = '^0.0.1'
+    fs.writeFileSync('package.json', JSON.stringify(newPkg))
+    fs.copySync('node_modules/admiral-unit-plugin', 'node_modules/admiral-unit-plugin2')
+    fs.copySync('admiralfile.js', '_admiralfile.js')
+    fs.removeSync('admiralfile.js')
 
     return admiral.execute().catch((e) => {
-      assert.equal(e.message, 'Must specify unit runner to use.');
+      assert.equal(e.message, 'Must specify unit runner to use.')
 
-      fs.removeSync('node_modules/admiral-unit-plugin2');
-      fs.writeFileSync('package.json', ogPkg);
-      fs.copySync('_admiralfile.js', 'admiralfile.js');
-      fs.removeSync('_admiralfile.js');
-    });
-  });
-
-});
+      fs.removeSync('node_modules/admiral-unit-plugin2')
+      fs.writeFileSync('package.json', ogPkg)
+      fs.copySync('_admiralfile.js', 'admiralfile.js')
+      fs.removeSync('_admiralfile.js')
+    })
+  })
+})
